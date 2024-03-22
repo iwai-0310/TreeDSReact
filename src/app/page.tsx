853a1271b91d18321bf1f4e4cf036b45d790b0d8
simpleton <chazesingh@gmail.com>
2024-03-22 23:45:37 +0530

    add simple node with placeholder data to TEST- react-d3-tree from npm --SUCCESS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+'use client'
 import Image from "next/image";
-
+import React,{useState} from "react";
+import {RawNodeDatum, Tree} from 'react-d3-tree'
 export default function Home() {
+  const [tree,setTree]=useState< RawNodeDatum | RawNodeDatum[] >({
+    name: 'root',
+    children:[],
+  });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Lets see if you can see this</h1>
-    </main>
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="100vw 100vh">
+      <Tree data={tree}/>
+    </div> 
+    // </main>
   );
 }
