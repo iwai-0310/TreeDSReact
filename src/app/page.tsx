3b9bba822e2a84d6d473555ff499d6f28d40d9c9
simpleton <chazesingh@gmail.com>
2024-03-23 15:23:02 +0530

    add form to modal to accept child name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,37 @@
-'use client'
+"use client";
 import Image from "next/image";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import dynamic from "next/dynamic";
 
-import {RawNodeDatum} from 'react-d3-tree'
+import { RawNodeDatum, TreeNodeDatum } from "react-d3-tree";
 import AddChildModal from "@/components/AddChildModal";
 import { Modal } from "react-responsive-modal";
 
-//dynamically import the tree 
-const Tree=dynamic(()=>import("react-d3-tree"),{
-  ssr:false,
-})
+//dynamically import the tree
+const Tree = dynamic(() => import("react-d3-tree"), {
+  ssr: false,
+});
 export default function Home() {
-
-  const [tree,setTree]=useState< RawNodeDatum | RawNodeDatum[] >({
-    name: 'root',
-    children:[],
+  const [tree, setTree] = useState<RawNodeDatum | RawNodeDatum[]>({
+    name: "root",
+    children: [],
   });
-
+  const [node, setNode] = useState<undefined | TreeNodeDatum>(undefined);
   const [open, setOpen] = useState(false);
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <div className="h-screen w-screen">
-      <Tree data={tree} rootNodeClassName="node__root"
-      onNodeClick={() =>setOpen(true)} translate={{
-        x:200,y:200,
-      }}/>
+      <Tree
+        data={tree}
+        rootNodeClassName="node__root rd3t-label__title "
+        onNodeClick={() => setOpen(true)}
+        translate={{
+          x: 200,
+          y: 200,
+        }}
+      />
       <AddChildModal open={open} setOpen={setOpen} />
-    </div> 
+    </div>
     // </main>
   );
 }
--- src/components/AddChildModal.tsx
+++ src/components/AddChildModal.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 //import responsive modal
- import 'react-responsive-modal/styles.css';
+import "react-responsive-modal/styles.css";
 import { Modal } from "react-responsive-modal";
 
-const AddChildModal = ({open,setOpen}) => {
+const AddChildModal = ({ open, setOpen }) => {
   // const [open, setOpen] = useState(false);
   //change state from close to open when open clicked
   const onOpenModal = () => setOpen(true);
@@ -13,9 +13,31 @@ const AddChildModal = ({open,setOpen}) => {
     <div>
       <button onClick={onOpenModal}>Open modal</button>
       <Modal open={open} onClose={onCloseModal}>
-        <h2>lets see if this modal works</h2>
-        {/* TBD later */}
-        {/* Add a form to add data to it */}
+        <h2>Add element:</h2>
+        <form>
+          <div className="mb-4">
+            <label
+              for="childElement"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Title
+            </label>
+            <input
+              id="childElement"
+              type="text"
+              placeholder="Enter child element name"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              +
+            </button>
+          </div>
+        </form>
       </Modal>
     </div>
   );
