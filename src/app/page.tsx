fa093f9369ef94e4f8fdd87fb703591e81ecaea5
simpleton <chazesingh@gmail.com>
2024-03-24 21:52:43 +0530

    add center position to node title using dimension prpo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,88 +24,180 @@ const containerStyles = {
 };
 
 export default function Home() {
-
-  
-  
   const [tree, setTree] = useState<RawNodeDatum | RawNodeDatum[]>({
     name: "DLC",
     children: [
       {
         name: "Research",
-        attributes: { data: "michael jackson was here" },
+        attributes: {
+          data: "Gather information on market trends, customer preferences, and competitor analysis. This includes data on demographics, purchasing behavior, and market size",
+          color: "blue",
+        },
         children: [
           {
             name: "External",
-            attributes: {},
-            children: [],
+            attributes: {
+              data: "Gather information on market trends, customer preferences, and competitor analysis. This includes data on demographics, purchasing behavior, and market size",
+              color: "blue",
+            },
+            children: [
+              {
+                name: "B2C",
+                attributes: {
+                  data: "Gather information on market trends, customer preferences, and competitor analysis. This includes data on demographics, purchasing behavior, and market size",
+                  color: "blue",
+                },
+                children: [
+                  {
+                    name: "Online",
+                    attributes: {
+                      data: "Gather information on market trends, customer preferences, and competitor analysis. This includes data on demographics, purchasing behavior, and market size",
+                      color: "blue",
+                    },
+                    children: [],
+                  },
+                  {
+                    name: "Interview",
+                    attributes: {
+                      data: "Gather information on market trends, customer preferences, and competitor analysis. This includes data on demographics, purchasing behavior, and market size",
+                      color: "blue",
+                    },
+                    children: [],
+                  },
+                  {
+                    name: "Public Data",
+                    attributes: {
+                      data: "Gather information on market trends, customer preferences, and competitor analysis. This includes data on demographics, purchasing behavior, and market size",
+                      color: "blue",
+                    },
+                    children: [],
+                  },
+                  {
+                    name: "Health",
+                    attributes: {
+                      data: "Gather information on market trends, customer preferences, and competitor analysis. This includes data on demographics, purchasing behavior, and market size",
+                      color: "blue",
+                    },
+                    children: [],
+                  },
+                ],
+              },
+              {
+                name: "B2C",
+                attributes: {
+                  data: "Gather information on market trends, customer preferences, and competitor analysis. This includes data on demographics, purchasing behavior, and market size",
+                  color: "blue",
+                },
+                children: [],
+              },
+            ],
           },
           {
             name: "Internal",
-            attributes: {},
+            attributes: {
+              data: "Gather information on market trends, customer preferences, and competitor analysis. This includes data on demographics, purchasing behavior, and market size",
+              color: "blue",
+            },
             children: [],
           },
         ],
       },
       {
         name: "Planning",
-        attributes: {},
+        attributes: {
+          data: "List of components and raw materials needed for production,Data on available resources (machinery, labor, etc.) and their utilization. Time required for procurement, production, and delivery",
+          color: "aqua",
+        },
         children: [
           {
             name: "PRD",
-            attributes: {},
+            attributes: {
+              data: "List of components and raw materials needed for production,Data on available resources (machinery, labor, etc.) and their utilization. Time required for procurement, production, and delivery",
+              color: "aqua",
+            },
             children: [],
           },
           {
             name: "Specs",
-            attributes: {},
+            attributes: {
+              data: "List of components and raw materials needed for production,Data on available resources (machinery, labor, etc.) and their utilization. Time required for procurement, production, and delivery",
+              color: "aqua",
+            },
             children: [],
           },
         ],
       },
       {
         name: "Designing",
-        attributes: {},
+        attributes: {
+          data: "Detailed specifications for the product, including dimensions, materials, and features,Measurements, CAD files, and design iterations",
+          color: "orange",
+        },
         children: [
           {
             name: "Hardware",
-            attributes: {},
+            attributes: {
+              data: "Detailed specifications for the product, including dimensions, materials, and features,Measurements, CAD files, and design iterations",
+              color: "orange",
+            },
             children: [],
           },
           {
             name: "Software",
-            attributes: {},
+            attributes: {
+              data: "Detailed specifications for the product, including dimensions, materials, and features,Measurements, CAD files, and design iterations",
+              color: "orange",
+            },
             children: [],
           },
         ],
       },
       {
         name: "Manufacturing",
-        attributes: {},
+        attributes: {
+          data: "Cycle Times: Time taken for each manufacturing step. Quality Control Metrics: Defect rates, yield, and rework data",
+          color: "pink",
+        },
         children: [
           {
             name: "Material",
-            attributes: {},
+            attributes: {
+              data: "Cycle Times: Time taken for each manufacturing step. Quality Control Metrics: Defect rates, yield, and rework data",
+              color: "pink",
+            },
             children: [],
           },
           {
             name: "Production",
-            attributes: {},
+            attributes: {
+              data: "Cycle Times: Time taken for each manufacturing step. Quality Control Metrics: Defect rates, yield, and rework data",
+              color: "pink",
+            },
             children: [],
           },
         ],
       },
       {
         name: "Sales/Manufacturing",
-        attributes: {},
+        attributes: {
+          data: "Customer Orders: Details of products ordered, quantities, and delivery dates.Invoices: Billing information.",
+          color: "violet",
+        },
         children: [
           {
             name: "Online",
-            attributes: {},
+            attributes: {
+              data: "Customer Orders: Details of products ordered, quantities, and delivery dates.Invoices: Billing information.",
+              color: "violet",
+            },
             children: [],
           },
           {
             name: "Dealership",
-            attributes: {},
+            attributes: {
+              data: "Customer Orders: Details of products ordered, quantities, and delivery dates.Invoices: Billing information.",
+              color: "violet",
+            },
             children: [],
           },
         ],
@@ -143,7 +235,7 @@ export default function Home() {
   // };
 
   const [node, setNode] = useState<TreeNodeDatum | undefined>(undefined);
-  const handleNodeClick=(nodeDatum)=>{
+  const handleNodeClick = (nodeDatum) => {
     setNode(nodeDatum);
   };
   const onCloseModal = () => setNode(undefined);
@@ -159,13 +251,21 @@ export default function Home() {
     x: 10,
     y: 10,
   };
+  const elem=document.getElementById("#pannel");
+  const bound=elem?.getBoundingClientRect();
+  const xCenter=0;
+  const yCenter=0;
+  if(bound){
+  const xCenter=(bound.left + bound.right) / 2;
+  const yCenter=(bound.top + bound.bottom) / 2;
+}
   //create a funciton to set height at which the tree renders
   const heightScreen = typeof window !== "undefined" ? window.innerHeight : 0;
-  const height = heightScreen / 2;
+  const heightForTranslate = heightScreen / 2;
 
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div className="h-screen w-screen">
+    <div className="h-screen w-screen " id="pannel">
       <Tree
         data={tree}
         // onNodeClick={(datum) => {
@@ -175,22 +275,24 @@ export default function Home() {
         // }}
         orientation="horizontal"
         pathFunc="step"
-        translate={{ x: 100, y: height }}
+        translate={{ x: xCenter, y: yCenter }}
         nodeSize={nodeSize}
+        dimensions={{width: 1400,height:500}}
         //collapsible={false}
-        renderCustomNodeElement={(r3dtProps)=>
-          renderNodeWithCustomEvents({...r3dtProps,handleNodeClick})
+        renderCustomNodeElement={(r3dtProps) =>
+          renderNodeWithCustomEvents({ ...r3dtProps, handleNodeClick })
         }
       />
       {/* <AddChildModal nameParent={node?.name}  onOpen={Boolean(node)} onClose={onCloseModal}
       onSubmit={handleSubmit}/> */}
       {node && (
-      <ShowParentDataModal
-        nodeName={node?.name}
-        onOpen={Boolean(node)}
-        onClose={onCloseModal}
-        nodeAtt={node?.attributes?.data}
-      />)}
+        <ShowParentDataModal
+          nodeName={node?.name}
+          onOpen={Boolean(node)}
+          onClose={onCloseModal}
+          nodeAtt={node?.attributes?.data}
+        />
+      )}
       {/* <Card dataAttibutes={node?.attributes} onOpen={Boolean(node)} onClose={onCloseModal}/> */}
     </div>
     // </main>
@@ -200,11 +302,25 @@ export default function Home() {
 const renderNodeWithCustomEvents = ({
   nodeDatum,
   toggleNode,
-  handleNodeClick
+  handleNodeClick,
 }) => (
   <g>
-    <circle r="5" onClick={() => handleNodeClick(nodeDatum)} />
-    <text fill="black" strokeWidth="1" x="-20" y="30" onClick={toggleNode}>
+    {/* <circle r="5" onClick={() => handleNodeClick(nodeDatum)} /> */}
+    <rect
+      width="20"
+      height="20"
+      y="-10"
+      fill={nodeDatum?.attributes?.color}
+      onClick={() => handleNodeClick(nodeDatum)}
+    />
+    <text
+      className="rectangle"
+      fill="black"
+      strokeWidth="1"
+      x="-20"
+      y="30"
+      onClick={toggleNode}
+    >
       {nodeDatum.name}
     </text>
   </g>
